5204271b1919ea1f4a03f4b465d6c0b452ac4d9d
Akshit Vig <akshitvig48@gmail.com>
2025-04-06 17:22:33 +0530

    dsd

--- app/page.tsx
+++ app/page.tsx
@@ -9,22 +9,24 @@ export default function Home() {
     <div className=" min-h-screen font-[family-name:var(--font-geist-sans)]">
       <Navbar />
 
-      <p className=" text-5xl font-bold text-center mt-24">Welcome to Enso</p>
+      <p className=" sm:text-5xl text-4xl font-bold text-center mt-24">
+        Welcome to Enso
+      </p>
       <div className=" flex justify-center">
-        <p className=" text-2xl  text-center mt-10 w-[700px] text-zinc-500">
+        <p className=" sm:text-2xl text-lg ml-4 mr-4 sm:ml-0 sm:mr-0 text-center mt-5 sm:mt-10 w-[700px] text-zinc-500">
           Create or join virtual whiteboards to draw, plan and brainstorm with
           your team in real-time.
         </p>
       </div>
 
-      <div className=" flex space-x-6 justify-center mt-16">
+      <div className=" flex sm:space-x-6 space-x-4 justify-center mt-10 sm:mt-16">
         <Link href={"/signup"}>
-          <button className="  transition-all  hover:bg-zinc-300 duration-300 active:scale-95 bg-white hover:cursor-pointer text-black py-3 px-4 rounded-md text-xl">
+          <button className="  transition-all  hover:bg-zinc-300 duration-300 active:scale-95 bg-white hover:cursor-pointer text-black py-3 px-4 rounded-md sm:text-xl">
             Get Started
           </button>
         </Link>
         <Link href={"/signin"}>
-          <button className="py-3 hover:bg-zinc-800  transition-all duration-300 active:scale-95 px-4 rounded-md text-xl hover:cursor-pointer border border-zinc-800">
+          <button className="py-3 hover:bg-zinc-800  transition-all duration-300 active:scale-95 px-4 rounded-md sm:text-xl hover:cursor-pointer border border-zinc-800">
             I already have an account
           </button>
         </Link>
--- components/ensopreview.tsx
+++ components/ensopreview.tsx
@@ -1,6 +1,6 @@
 export default function Preview() {
   return (
-    <div className="flex mt-16 mb-16  justify-center">
+    <div className="flex ml-4 mr-4 sm:ml-0 sm:mr-0 mt-16 mb-16  justify-center">
       <div className="flex justify-center w-[1300px]  bg-transparent">
         <video src="/preview2.mp4" autoPlay muted loop playsInline></video>
       </div>
