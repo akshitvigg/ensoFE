724a37617e5e4b9ea26057a12a3a4e00b5c18f33
Akshit Vig <akshitvig48@gmail.com>
2025-07-13 20:50:46 +0530

    aos duration changed

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     Aos.init({
-      duration: 500,
+      duration: 800,
       once: true,
     });
   });
